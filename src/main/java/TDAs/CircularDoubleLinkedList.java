5ca9e31d758bbd0d40ce9d96bf8da6f858edf3af
lizVergara <lizvcast@espol.edu.ec>
2021-06-25 21:50:12 -0500

    CircularDoubleLinkedList

---
+++ src/main/java/TDAs/CircularDoubleLinkedList.java
@@ -0,0 +1,120 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TDAs;
+
+import TDAs.CircularDoubleNode;
+import java.util.Iterator;
+
+/**
+ *
+ * @author damar
+ */
+public class CircularDoubleLinkedList <E> implements List<E> {
+    private int efectivo;
+    private CircularDoubleNode <E> previous;
+    private CircularDoubleNode <E> next;
+
+    public CircularDoubleLinkedList(int efectivo, CircularDoubleNode<E> last) {
+        efectivo = efectivo;
+        previous = null;
+        next = null;
+    }
+
+    @Override
+    public boolean addFirst(E element) {
+         CircularDoubleNode<E> nodo = new CircularDoubleNode<>(element);
+        if (element == null) {
+            return false;
+        } else if (isEmpty()) {
+            previous = next = nodo;
+        }
+//        } else {
+//            nodo.setNext(first);
+//            first = nodo;
+//        }
+//        efectivo++;
+//        return true;
+        return false;
+        
+    }
+
+    @Override
+    public boolean addLast(E element) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean removeFirst() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean removeLast() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public E getFirst() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public E getLast() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean insert(int index, E element) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean contains(E element) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public E get(int index) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int indexOf(E element) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean isEmpty() {
+        
+        return (next == null && previous == null);
+    }
+
+    @Override
+    public E remove(int index) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean remove(E element) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public E set(int index, E element) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int size() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
