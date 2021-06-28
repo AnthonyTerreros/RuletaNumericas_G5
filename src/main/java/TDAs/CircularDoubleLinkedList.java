b668a6012076d77be547af3559863ffd4dc86ee7
lizVergara <lizvcast@espol.edu.ec>
2021-06-28 00:12:16 -0500

    CircularDouble metodos terminados

--- src/main/java/TDAs/CircularDoubleLinkedList.java
+++ src/main/java/TDAs/CircularDoubleLinkedList.java
@@ -19,12 +19,36 @@ public class CircularDoubleLinkedList<E> implements List<E> {
 
     @Override
     public void clear() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        last = null;
     }
 
     @Override
     public void add(int index, E element) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        int c = 0;
+        CircularDoubleNode<E> nodo = new CircularDoubleNode<>(element);
+        if (element == null) {
+            System.out.println("valor ingresado invalido");
+        } else if (index == 0) {
+            addFirst(element);
+        } else if (index == efectivo) {
+            addLast(element);
+
+        } else if (index < 0 || index > efectivo) {
+            System.out.println("Index invalido");
+
+        } else {
+
+            for (CircularDoubleNode<E> i = last.getNext(); i != last; i = i.getNext()) {
+                if (c == index - 1) {
+                    nodo.setNext(i.getNext());
+                    i.setNext(nodo);
+                    nodo.setPrevious(i);
+                    nodo.getNext().setPrevious(nodo);
+                    efectivo++;
+                }
+                c++;
+            }
+        }
     }
 
     public CircularDoubleLinkedList() {
@@ -42,15 +66,15 @@ public class CircularDoubleLinkedList<E> implements List<E> {
             last = nodo;
             nodo.setNext(nodo);
             nodo.setPrevious(nodo);
-
+            efectivo++;
         } else {
+            (last.getPrevious()).setPrevious(nodo);
             nodo.setNext(last.getNext());
             nodo.setPrevious(last);
-            (last.getPrevious()).setNext(nodo);
+            last.setNext(nodo);
+            efectivo++;
         }
-        efectivo++;
         return true;
-
     }
 
     @Override
@@ -63,29 +87,54 @@ public class CircularDoubleLinkedList<E> implements List<E> {
             addFirst(element);
 
         } else {
-            nodo.setPrevious(last);
+            (last.getPrevious()).setPrevious(nodo);
             nodo.setNext(last.getNext());
+            nodo.setPrevious(last);
             last.setNext(nodo);
             last = nodo;
+            efectivo++;
         }
-        efectivo--;
+
         return true;
 
     }
 
     @Override
     public E removeFirst() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if (isEmpty()) {
+            return null;
+        } else if (efectivo == 1) {
+            last = null;
+        } else {
+            E d = (last.getNext()).getContent();
+            ((last.getNext()).getNext()).setPrevious(last);
+            last.setNext((last.getNext()).getNext());
+            efectivo--;
+            return d;
+        }
+
+        return null;
     }
 
     @Override
     public E removeLast() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if (last == null) {
+            return last.getContent();
+        }
+        CircularDoubleNode<E> temp = last.getPrevious();
+        temp.setNext(last.getNext());
+        last.getNext().setPrevious(temp);
+        last = temp;
+        return last.getContent();
     }
 
     @Override
     public E get(int index) {
         int c = 0;
+        if (index == efectivo - 1) {
+            return last.getContent();
+
+        }
         for (CircularDoubleNode<E> i = last.getNext(); i != last; i = i.getNext()) {
             if (c == index) {
                 return i.getContent();
@@ -98,39 +147,58 @@ public class CircularDoubleLinkedList<E> implements List<E> {
 
     @Override
     public boolean isEmpty() {
-
         return (last == null);
     }
 
-    @Override
+    @Override//corregir
     public E remove(int index) {
         int c = 0;
-        E u = last.getContent();
+        if (index < 0 || index >= efectivo) {
+            return null;
+        } else if (index == 0) {
+            E tmp = (last.getNext()).getContent();
+            removeFirst();
+            return tmp;
+        } else if (index == efectivo - 1) {
+            E tmp = last.getContent();
+            removeLast();
+            
+            return tmp;
+        }
         for (CircularDoubleNode<E> i = last.getNext(); i != last; i = i.getNext()) {
             if (c == index) {
                 E d = i.getContent();
                 (i.getPrevious()).setNext(i.getNext());
                 (i.getNext()).setPrevious(i.getPrevious());
-                System.out.println(c + " " + index);
+                efectivo--;
                 return d;
             }
             c++;
+            
         }
-        return u;
+        return null;
     }
 
     @Override
     public E set(int index, E element) {
-        int c = 0;
-        for (CircularDoubleNode<E> i = last.getNext(); i != last; i = i.getNext()) {
-            if (c == index) {
-                E d = i.getContent();
-                i.setContent(element);
-                return d;
+        if (index < 0 || index > efectivo) {
+            return null;
+        } else if (element == null) {
+            System.out.println("elemento invalido");
+            return null;
+
+        } else {
+            int c = 0;
+            for (CircularDoubleNode<E> i = last.getNext(); i != last; i = i.getNext()) {
+                if (c == index) {
+                    E d = i.getContent();
+                    i.setContent(element);
+                    return d;
+                }
+                c++;
             }
-            c++;
+            return null;
         }
-        return null;
     }
 
     @Override
